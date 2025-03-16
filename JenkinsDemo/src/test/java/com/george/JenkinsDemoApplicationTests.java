bc418b833591e4d21b8401f962ee951dcf8ce3c9
Jujuwryy <georgemahfoud2@gmail.com>
2025-03-16 23:04:06 +0300

    d

---
+++ JenkinsDemo/src/test/java/com/george/JenkinsDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.george;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Testing context loading");
+    }
+}
